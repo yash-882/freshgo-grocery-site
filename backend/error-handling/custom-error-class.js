e2b98c9a2f543689f16d965eb5a9c2b5d4ac59e2
yash <yashh2nd@gmail.com>
2025-07-24 16:15:51 +0530

    feat(Error-Handling): Add custom-error-class.js to create operational errors

---
+++ backend/error-handling/custom-error-class.js
@@ -0,0 +1,13 @@
+// Custom error class for creating operational errors
+class CustomError extends Error {
+  constructor(name, message, statusCode) {
+    super(message);
+    this.name = name;
+    this.message = message;
+    this.statusCode = statusCode;
+    // This is an operational error, means caused by user input or application logic
+    this.isOperational = true; 
+    // add stack trace for debbugging 
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
