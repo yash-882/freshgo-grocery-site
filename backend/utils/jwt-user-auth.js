e05b73cc246f63f48e051ea0f95d5bbfbc100757
yash <yashh2nd@gmail.com>
2025-07-24 17:55:30 +0530

    feat(util-auth): verify JWT access/refresh tokens and re-issue new access tokens(after validating refresh token) if expired

--- backend/utils/jwt-user-auth.js
+++ backend/utils/jwt-user-auth.js
@@ -19,3 +19,51 @@ export function signRefreshToken(payload) {
 }
 
 
+// verify access token
+export function verifyAccessToken(accessToken, refreshToken) {
+    // for catching token expiration error and provide a new access token
+    // after successful verification of refresh token
+    try {
+
+        // validate access token
+        const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+
+        // validation successful, return decoded token
+        return {
+            isReissued: false, //token is not re-issued, means it was valid
+            decoded //decoded token
+        }
+
+    } catch (err) {
+
+        if (err.name === 'TokenExpiredError') {
+            // throws error on all JWT verification errors
+            verifyRefreshToken(refreshToken)
+
+            //refresh token was valid, decode accessToken 
+            const payload = jwt.decode(accessToken)
+
+            // sign new access token
+            const newToken = signAccessToken(payload)
+
+            // return new access token
+            // and indicate that the token was re-issued
+            return {
+                isReissued: true,
+                newToken 
+            }
+        }
+
+        // if any JWT error except TokenExpiredError occurs, throw it
+        throw err;
+    }
+}
+
+export function verifyRefreshToken(token) {
+    // validate refresh token
+    const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+
+    // validation successful, return decoded token
+    return decoded
+}
+
