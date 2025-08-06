5c3e112c43e39929c0d4c5a9c0a546e329252056
yash <yashh2nd@gmail.com>
2025-08-06 14:33:19 +0530

    feat(auth): add /login route with handler

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 import controllerWrapper from "../utils/controller-wrapper.js";
+import bcrypt from 'bcrypt';
 import UserModel from "../models/user-model.js";
 import CustomError from "../error-handling/custom-error-class.js";
 import { signAccessToken, signRefreshToken } from "../utils/jwt-user-auth.js";
@@ -66,4 +67,72 @@ export const signUp = controllerWrapper(async (req, res, next) => {
             user: createdUser
         }
     });
+})
+
+// login controller
+export const login = controllerWrapper(async (req, res, next) => {
+    const {email, password} = req.body || {};
+
+    // if any required field is missing, throw an error
+    if(!email || !password)
+        return next(
+    new CustomError('BadRequestError', 'Please enter all required fields!', 400));
+
+    const user  = await UserModel.findOne({email});
+
+// if user is not already registered/signed-up
+    if(!user) {
+        return next(
+ new CustomError('NotFoundError', 'Email is incorrect or not registered with us', 404));
+    }
+
+    const isPasswordCorrect =  await bcrypt.compare(password, user.password)
+
+    if(!isPasswordCorrect){
+        return next(
+    new CustomError('UnauthorizedError', 'Incorrect password!', 401));
+    }
+
+    // password was correct, sign tokens
+    // tokens properties AT = Access Token, RT = Refresh Token
+    const tokens = {
+        AT: signAccessToken({
+            id: user._id, 
+            role: user.role
+        }),
+        RT: signRefreshToken({
+            id: user._id, 
+            role: user.role
+        }),
+
+    // parseInt stops parsing when 'd'(stands for days) is triggered,
+    // and returns numbers of days in Number datatype
+        AT_AGE: parseInt(process.env.ACCESS_TOKEN_EXPIRES_IN), //in minutes
+        RT_AGE: parseInt(process.env.REFRESH_TOKEN_EXPIRES_IN) //in days
+    } 
+
+    // store tokens in the browser cookies
+    res.cookie('AT', tokens.AT, {
+        httpOnly: true,
+        sameSite: 'strict',
+        expires: new Date(Date.now() + tokens.AT_AGE * 60 * 1000), // minutes 
+    });
+
+    res.cookie('RT', tokens.RT, {
+        httpOnly: true,
+        sameSite: 'strict',
+        expires: new Date(Date.now() + tokens.RT_AGE * 24 * 60 * 60 * 1000), // days
+    });
+
+    // delete password before responding
+    user.password = undefined
+
+    //logged in successfully, send response
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user
+        }
+    });
+
 })
