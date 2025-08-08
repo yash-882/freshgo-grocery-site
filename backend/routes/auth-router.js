d22a4a5ee76e128801d5cd43cc54e44b598a167f
yash <yashh2nd@gmail.com>
2025-08-08 17:03:50 +0530

    feat(auth): add logic (in authorizeUser) to avoid login/signup if already authenticated

--- backend/routes/auth-router.js
+++ backend/routes/auth-router.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { login, signUp } from '../controllers/auth-controller.js';
+import { authorizeUser, login, signUp } from '../controllers/auth-controller.js';
 
 
 const authRouter = Router();
+
+// middleware to authorize user and allow access to protected routes
+// additionally, it avoids login/signup requests if user is already logged in
+authRouter.use(authorizeUser)
 
 // sign-up route
 authRouter.route('/sign-up')
