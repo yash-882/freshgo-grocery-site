7ca9d522c19ee0fad094b5f5110041aababa801b
yash <yashh2nd@gmail.com>
2025-08-11 19:42:12 +0530

    feat(auth): add route /change-password with handler

--- backend/utils/auth-helpers.js
+++ backend/utils/auth-helpers.js
@@ -1,5 +1,6 @@
 import CustomError from "../error-handling/custom-error-class.js";
 import UserModel from "../models/user-model.js";
+import bcrypt from 'bcrypt';
 
 // Function to get user by query or throw an error if not found
 export const findUserByQuery = async (query, 
@@ -13,4 +14,17 @@ export const findUserByQuery = async (query,
     }
 
     return user;
+}
+
+// validates password by comparing plain text password with hashed password
+export const isPasswordCorrect = async ({plain, hashed}, errMessage = 'Incorrect password!') => {
+
+    // compare plain password with hashed password
+    const isPasswordCorrect = await bcrypt.compare(plain, hashed);
+
+    if(!isPasswordCorrect) {
+        throw new CustomError('UnauthorizedError', errMessage, 401);
+    }
+
+    return true;
 }
