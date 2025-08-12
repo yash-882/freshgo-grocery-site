c8f5eccb12081cc98184857580a744b5a51259ab
yash <yashh2nd@gmail.com>
2025-08-12 15:33:32 +0530

    feat(auth): add sendEmail utility to send emails via smtp server using nodemailer package

---
+++ backend/utils/mailer.js
@@ -0,0 +1,26 @@
+import mailer from 'nodemailer';
+
+// create a transporter object using SMTP transport
+const transporter = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL,
+        pass: process.env.GOOGLE_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: process.env.NODE_ENV === 'production' //NEVER SET IT TO FALSE IN PRODUCTION
+    },
+})
+
+async function sendEmail(to, subject, text) {
+
+    // sending email...
+    await transporter.sendMail({
+        from: 'FreshGo Support <FromFreshGo>' , //'FreshGo Support' will be displayed as a sender
+        to, //email address of the recipient,
+        subject, // subject of the email
+        text// body of the email
+    });
+}
+
+export default sendEmail;
