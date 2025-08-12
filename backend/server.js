01f954ca9fc4437dcc4084a50bc36623b59b48ed
yash <yashh2nd@gmail.com>
2025-08-12 18:42:27 +0530

    feat(redis): add redis client setup for data caching and storing it temporary

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import './configs/load-env.js' //load environment variables
 import mongoose from 'mongoose'
+import redisClient from './configs/redis-client.js'
 import app from './app.js'
+
+// listens for Redis client errors
+redisClient.on('error', (err) => console.error('Redis Client Error', err))
+
+// connect to Redis
+redisClient.connect()
+  .then(() => console.log('Connected to Redis successfully'))
+  .catch(err => console.error('Redis connection error:', err))
 
 // connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(console.log(`Connected to MongoDB successfully`))
+  .then(() => console.log(`Connected to MongoDB successfully`))
   .catch(err => console.error('MongoDB connection error:', err))
 
 // Listen on specified port
