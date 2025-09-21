6985994d84ba4e7b00d9069587e187f282fa4edc
yash <yashh2nd@gmail.com>
2025-09-21 19:42:51 +0530

    feat(auth): implement Google OAuth2 strategy with passport.js for passwordless sign-in

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,22 @@ const app = express();
 
 // import routers
 import authRouter from './routes/auth-router.js';
+import productRouter from './routes/product-route.js';
+import cartRouter from './routes/cart-route.js';
+import adminRouter from './routes/admin-route.js';
+import userRouter from './routes/user-route.js';
+
+// auth strategies
+import googleAuth from './auth-strategies/google-auth.js';
 
 // error custom module
 import GlobalErrorHandler from './error-handling/global-error-handler.js';
 
 // npm packages
+import passport from 'passport';
 import cookieParser from 'cookie-parser';
-import adminRouter from './routes/admin-route.js';
-import userRouter from './routes/user-route.js';
-import productRouter from './routes/product-route.js';
 import qs from 'qs';
-import cartRouter from './routes/cart-route.js';
+
 
 // parses query strings ("?price[gt]=20&sort=-price" -> {price: {gt: "20"}, sort="-price"})
 app.set("query parser", (query)=> {
@@ -25,6 +30,12 @@ app.use(express.json())
 
 // parse cookies
 app.use(cookieParser())
+
+// initialize passport
+app.use(passport.initialize())
+
+// google OAUTH2
+passport.use(googleAuth)
 
 // auth router
 app.use('/api/auth', authRouter)
