9d6fe8ea8dbf8d26dbd09085bc2b069d47cc4166
yash <yashh2nd@gmail.com>
2025-10-16 00:36:59 +0530

    feat(configs): add retry strategy for Redis client

--- backend/configs/ioredis-client.js
+++ backend/configs/ioredis-client.js
@@ -6,6 +6,11 @@ const IOredisClient = new IOredis({
     password: process.env.REDIS_PASSWORD,
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    retryStrategy: (times) => {
+        // reconnect after 
+        // (exponential delay before each retry: 50 -> 100 -> 150 -> ...upto 2000ms)
+        return Math.min(times * 50, 2000) 
+    },
     maxRetriesPerRequest: null,
 })
 
